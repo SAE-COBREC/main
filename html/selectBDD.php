c34ae243d86adc4bb3101c05f265f666744711b4
Mls <marceaulesech@gmail.com>
2025-11-12 16:17:17 +0100

    c'estr parfait ?

--- html/selectBDD.php
+++ html/selectBDD.php
@@ -3,15 +3,15 @@
 include("config1.php");
 
 try {
-    $dbh = new PDO("$driver:host=$serveur;dbname=$dbname", $user, $pass);
+    $pdo = new PDO("$driver:host=$serveur;dbname=$dbname", $user, $pass);
     
-    foreach ($dbh->query(query: "SELECT * FROM cobrec1._compte", fetchMode: PDO::FETCH_ASSOC) as $row) {
+    foreach ($pdo->query(query: "SELECT * FROM cobrec1._compte", fetchMode: PDO::FETCH_ASSOC) as $row) {
         echo "<pre>";
         print_r($row);
         echo "<pre>";
     }
 
-    $dbh = null;
+    $pdo = null;
 } catch (PDOException $e) {
     die("❌ Erreur de connexion : " . $e->getMessage());
 }
