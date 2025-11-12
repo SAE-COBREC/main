118ee3a72f48ac6f310e9358930a9e1e41db1558
Mls <marceaulesech@gmail.com>
2025-11-12 14:45:18 +0100

    Update selectBDD.php

--- html/selectBDD.php
+++ html/selectBDD.php
@@ -5,7 +5,7 @@ include("config.php");
 try {
     $dbh = new PDO("$driver:host=$serveur;dbname=$dbname", $user, $pass);
     
-    foreach ($dbh->query(query: "SELECT * FROM sae34.cobrec1._compte", fetchMode: PDO::FETCH_ASSOC) as $row) {
+    foreach ($dbh->query(query: "SELECT * FROM cobrec1._compte", fetchMode: PDO::FETCH_ASSOC) as $row) {
         echo "<pre>";
         print_r($row);
         echo "<pre>";
