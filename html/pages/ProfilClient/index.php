0510ea79af3612564d7baf6768ce0d7b80e61186
Mls <marceaulesech@gmail.com>
2025-11-13 17:37:59 +0100

    Update index.php

--- html/pages/ProfilClient/index.php
+++ html/pages/ProfilClient/index.php
@@ -231,7 +231,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <span data-type="profil">
                             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
                                 <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                                <circle cx="12" cy="7" r="4"4"></circle>
+                                <circle cx="12" cy="7" r="4"></circle>
                             </svg>
                         </span>
                     </header>
