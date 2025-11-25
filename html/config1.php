2767f340d31e393d36e949f6a5cef395a542384e
Léo <leotessier0987@gmail.com>
2025-11-25 10:56:01 +0100

    conf

--- html/config1.php
+++ html/config1.php
@@ -1,8 +1,7 @@
 <?php
-$serveur = '127.0.0.1';
+$serveur = '10.253.5.101';
 $driver = 'pgsql';
-$dbname = 'pgleo';
-$user = 'leo';
-$pass = 'leo';
+$dbname = 'saedb';
+$user = 'sae';
+$pass = 'kira13';
 $port = 5432;
-
