74177e2df7f7922f9402b566f2efed03dc8fb481
Mls <marceaulesech@gmail.com>
2025-11-25 15:33:44 +0100

    Update index.php

--- html/pages/ProfilClient/index.php
+++ html/pages/ProfilClient/index.php
@@ -211,7 +211,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Profil - Alizon</title>
     <link rel="stylesheet" href="/styles/ProfilClient/style.css" />
-    <!-- <link rel="preconnect" href="https://fonts.googleapis.com"> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="icon" type="image/png" href="../../img/favicon.svg">
     <!-- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> -->
     <!-- <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
