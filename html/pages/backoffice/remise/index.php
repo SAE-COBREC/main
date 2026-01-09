32ac9456344138d4821dd8cf20fd22d348418721
mudinyver <mael.udin--yver@etudiant.univ-rennes.fr>
2026-01-09 14:04:07 +0100

    serv

--- html/pages/backoffice/remise/index.php
+++ html/pages/backoffice/remise/index.php
@@ -28,11 +28,11 @@
                         $_SESSION["remise"]['_GET'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         if (empty($_SESSION["remise"]['_GET'][0])){
                             $_SESSION["remise"]['_GET'][0] = [];
-                            $_SESSION["remise"]['_GET'][0]['produit'] = '';
-                            $_SESSION["remise"]['_GET']['produit'] = '';
-                            $_SESSION["remise"]['_GET']['id_produit'] = '';
-                            $_SESSION["remise"]['_GET']['id_vendeur'] = [];
-                            $_SESSION["remise"]['_GET']['id_vendeur']['id_vendeur'] = '';
+                            $_SESSION["remise"]['_GET'][0]['produit'] = 0;
+                            $_SESSION["remise"]['_GET']['produit'] = 0;
+                            $_SESSION["remise"]['_GET']['id_produit'] = 0;
+                            $_SESSION["remise"]['_GET']['id_vendeur'] = 0;
+                            $_SESSION["remise"]['_GET']['id_vendeur']['id_vendeur'] = 0;
                         }
                         $_SESSION["remise"]['_GET'] = $_SESSION["remise"]['_GET'][0];
                         if (!empty($_SESSION["remise"]['_GET']['id_produit'])){
