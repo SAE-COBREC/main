eb3b65a31ee340c33b92f4f8579a8ed17ebfe16b
Mls <marceaulesech@gmail.com>
2026-01-12 16:34:08 +0100

    Update fonctions.php

--- html/pages/fonctions.php
+++ html/pages/fonctions.php
@@ -1269,7 +1269,7 @@ function trierProduits($listeProduits, $tri_par)
                 $comparison = $nombreVentesB <=> $nombreVentesA;
                 
                 if ($comparison === 0) {
-                    return $produitA['id_produit'] <=> $produitB['idproduit'];
+                    return $produitA['id_produit'] <=> $produitB['id_produit'];
                 }
                 
                 return $comparison;
