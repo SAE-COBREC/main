2c72b9edfc49538eb7011f63abfa78b42f013eb7
Léo <leotessier0987@gmail.com>
2026-01-13 16:58:30 +0100

    commit

--- Delivraptor/script.bash
+++ Delivraptor/script.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
-SIGNAL_FILE="/tmp/php_signal.flag"
-echo "UPDATE"
-echo "STATUS_UP 9" | nc -q 1 127.0.0.1 9000
+echo "STATUS_UP 10" | nc -q 1 127.0.0.1 9000
+echo "STATUS_UP 11" | nc -q 1 127.0.0.1 9000
+echo "STATUS_UP 12" | nc -q 1 127.0.0.1 9000
+echo "STATUS_UP 13" | nc -q 1 127.0.0.1 9000
+echo "STATUS_UP 14" | nc -q 1 127.0.0.1 9000
+echo "STATUS_UP 15" | nc -q 1 127.0.0.1 9000
+echo "STATUS_UP 16" | nc -q 1 127.0.0.1 9000
+echo "STATUS_UP 17" | nc -q 1 127.0.0.1 9000
