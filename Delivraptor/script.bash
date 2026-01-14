b2ea5a4bd3b1832a2cf68736262bc2299389d4d6
Léo <leotessier0987@gmail.com>
2026-01-14 14:32:54 +0100

    push

--- Delivraptor/script.bash
+++ Delivraptor/script.bash
@@ -1,10 +1,1 @@
 #!/bin/bash
-echo "bonjour"
-echo "STATUS_UP 10" | nc -q 1 127.0.0.1 9000
-echo "STATUS_UP 11" | nc -q 1 127.0.0.1 9000
-echo "STATUS_UP 12" | nc -q 1 127.0.0.1 9000
-echo "STATUS_UP 13" | nc -q 1 127.0.0.1 9000
-echo "STATUS_UP 14" | nc -q 1 127.0.0.1 9000
-echo "STATUS_UP 15" | nc -q 1 127.0.0.1 9000
-echo "STATUS_UP 16" | nc -q 1 127.0.0.1 9000
-echo "STATUS_UP 17" | nc -q 1 127.0.0.1 9000
