ea9a982f3516cd3c92f8dc2e48c23791ffb48c65
Mls <marceaulesech@gmail.com>
2026-03-02 17:33:18 +0100

    Update carte.php

--- html/partials/carte.php
+++ html/partials/carte.php
@@ -52,10 +52,10 @@ var map = L.map('map', {
     fullscreenControl: true
 }).setView([48.733333, -3.466667], 13);
 
-var Esri_WorldImagery = L.tileLayer(
-    'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-    }).addTo(map);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
 var adressesVendeurs = <?php echo json_encode($adresseDesVendeurs); ?>;
 
