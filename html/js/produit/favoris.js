39f4fded9d7ad6e549c6bdecb9e708ddeb8090e3
Gaetan LOLLIERIC <143171645+gaetanlollieric@users.noreply.github.com>
2026-03-09 14:40:38 +0100

    favoris ,notification

--- html/js/produit/favoris.js
+++ html/js/produit/favoris.js
@@ -6,12 +6,14 @@ function ajoutSuppFavoris(idProduit) {
         .then(data => {
             if (data.succes) {
                 if (data.action === 'ajoute') { //si la réponse de php est ajouté 
-                    btn.textContent = "Retirer des favoris";
+                    btn.textContent = "Retirer des favoris"; //on écrit Retirer des favoris dans le button
+                    notify("Ajouter aux favoris", 'success');//on notify le client
                 } else {
-                    btn.textContent = "Ajouter aux favoris";
+                    btn.textContent = "Ajouter aux favoris"; //on écrit Ajouter aux favoris dans le button
+                    notify("Retirer des favoris", 'info');   //on notify le client
                 }
             } else if (data.error === 'not_logged_in') { //si la réponse est not_logged_in alors on alert
-                alert("Vous devez être connecté pour ajouter des favoris.");
+                notify("Vous devez être connecté pour ajouter des favoris.", 'warning');
             }
         })
         .catch(err => console.error("Erreur page favoris:", err));
