0bf3a155263931cad1b27c78a07896a2702b86a7
mudinyver <mael.udin--yver@etudiant.univ-rennes.fr>
2026-03-16 09:32:03 +0100

    OTP cre et connex compte vendeur f()nel

---
+++ html/pages/backoffice/connexionVendeur/connexion_a2f.php
@@ -0,0 +1,80 @@
+<?php 
+session_start(); 
+require_once(__DIR__."/../../../vendor/autoload.php");
+use OTPHP\TOTP;
+use OTPHP\Factory;
+$hasError = false;
+?>
+<?php if (!empty($_SESSION['A2Fvendeur'])){ ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion A2F - Alizon</title>
+    <link rel="icon" type="image/png" href="../../../img/favicon.svg">
+    <link rel="stylesheet" href="../../../styles/Connexion_Creation/styleCoCrea.css">
+    <link rel="stylesheet" href="../../../styles/Connexion_Creation/vendeurCoCrea.css">
+</head>
+<body>
+    <main>
+        <div class="card">
+            <div class="logo">
+                <img src="../../../img/svg/logo-text.svg" alt="Logo Alizon"
+                    onclick="window.location.href='../index.php'">
+            </div>
+            <form action="connexion_a2f.php" method="post" enctype="multipart/form-data">
+                <h1>Connexion à double facteurs</h1>
+                <input type="text" inputmode="numeric" pattern="[0-9]{3} [0-9]{3}" placeholder="123 456" name="code" />
+                <div class="connex-btn">
+                    <button type="submit">
+                        Valider
+                    </button>
+                </div>
+                <div class="error">
+                    <?php if ($hasError){ 
+                        print_r("ERREUR OK"); ?>
+                        <strong>Erreur</strong> : Code A2F incorrect.
+                    <?php } ?>
+                </div>
+            </form>
+        </div>
+    </main>
+</body>
+</html>
+<script>
+    const inputCarte = document.querySelector("input");
+    inputCarte.addEventListener("input", function() {
+        let valeur = this.value;
+        valeur = valeur.replace(/\D/g, "");
+        valeur = valeur.replace(/(.{3})/g, "$1 ").trim();
+        this.value = valeur;
+    });
+</script>
+    <?php
+    $otp = TOTP::createFromSecret($_SESSION['A2Fvendeur']['secret_otp']);
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        print_r($otp->now());
+        if ($otp->verify(str_replace(' ', '', $_POST['code']), null, 20)){
+            $_SESSION['vendeur_id'] = $_SESSION['A2Fvendeur']['vendeur_id'];
+            $_SESSION['idCompte'] = $_SESSION['A2Fvendeur']['idCompte'];
+            $_SESSION['OTPvendeur']['secret'] = $_SESSION['A2Fvendeur']['secret_otp'];
+            unset($_SESSION['A2Fvendeur']);
+            $_SESSION['OTPvendeur']['statut'] = 'active';
+            $url = '../index.php';
+            echo '<!doctype html><html lang="fr"><head><meta http-equiv="refresh" content="0;url='.$url.'">';
+        }else{
+            $hasError = true;
+        }
+    }
+    
+    ?>
+<?php
+}else{
+    $url = '../index.php';
+    echo '<!doctype html><html lang="fr"><head><meta http-equiv="refresh" content="0;url='.$url.'">';
+}
+
+
+?>
